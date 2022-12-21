97b7919c98d523c8ea7ed8fff2de7f32bb5ce1a3
mohamedgamal246 <121165901+mohamedgamal246@users.noreply.github.com>
2022-12-21 13:57:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a number:");
+int num = int.Parse(Console.ReadLine());
+int i = 0;
+bool x = true;
+for (i = 1; i <= 100; i++)
+{
+    for (int j = 2; j < i; j++)
+
+        if (i % j == 0)
+        {
+            bool X = false;
+        }
+
+}
+if (x == true && i > 1)
+{
+    Console.WriteLine(" isprime\n");
+}
